56233619e009e0092080a2dcb8cbed26ad1235eb
Холостова Арина <airsha2004@gmail.com>
2025-02-17 17:52:31 +0300

    Arina commit
    kkkk

---
+++ SimpleCalculator/src/simplecalculator/Calculator.java
@@ -0,0 +1,14 @@
+
+package simplecalculator;
+
+
+public class Calculator {
+    
+    public static double add(double a, double b){
+        return a + b;
+    }
+    
+    public static double subtract(double a, double b){
+        return a - b;
+    }
+}
---
+++ SimpleCalculator/src/simplecalculator/CalculatorApp.java
@@ -0,0 +1,14 @@
+
+package simplecalculator;
+
+
+public class CalculatorApp {
+
+    
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(1.9, 1.2));
+        System.out.println(calc.subtract(1.9, 1.2));
+    }
+    
+}
